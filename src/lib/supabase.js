48643a90ada915ed9f93935197867b1c15f9cb9b
Corey Mangold <coreymangold@Coreys-MacBook-Pro.local>
2026-01-29 16:09:45 -0500

    Set flowType to pkce

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -1,17 +1,14 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY
-
-if (!supabaseUrl || !supabaseAnonKey) {
-  throw new Error('Missing Supabase environment variables')
-}
+const supabaseUrl = 'https://jmlxpcnkovxmadbygolp.supabase.co'
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImptbHhwY25rb3Z4bWFkYnlnb2xwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mzc1NjY2MzYsImV4cCI6MjA1MzE0MjYzNn0.Z60KJLbYYbFsjl4NJIKBPPwkqp8WEQULR79foZJAcuQ'
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
   auth: {
-    storageKey: 'mana88-auth',
+    storageKey: 'mana88-login-auth',
     autoRefreshToken: true,
     persistSession: true,
-    detectSessionInUrl: true
+    detectSessionInUrl: true,
+    flowType: 'pkce'
   }
 })
